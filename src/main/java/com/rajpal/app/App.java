b489cc28623e0dae4c112768f3c892b80b101c65
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-05-04 18:21:31 +0530

    class extraction done

--- src/main/java/com/rajpal/app/App.java
+++ src/main/java/com/rajpal/app/App.java
@@ -1,5 +1,14 @@
 package com.rajpal.app;
 
+import java.util.Collection;
+import java.util.List;
+
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.transform.BodyInterceptor;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.views.JavaView;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String tcdir = "src/testcases";
+        // AnalysisInputLocation inputLocation = new JavaSourcePathAnalysisInputLocation(tcdir);
+        AnalysisInputLocation inputLocation = new JavaClassPathAnalysisInputLocation(tcdir);
+        JavaView view = new JavaView(inputLocation);
+        List<BodyInterceptor> bodyInterceptors = inputLocation.getBodyInterceptors();
+        for(BodyInterceptor bodyInterceptor:bodyInterceptors){
+            System.out.println(bodyInterceptor.toString());
+        }
+        System.out.println("size of bil: "+bodyInterceptors.size());
+         Collection<JavaSootClass> classes = view.getClasses();
+         for(JavaSootClass clazz:classes){
+            System.out.println(clazz.getName().toString());
+         }
     }
 }
---
+++ src/testcases/T1.java
@@ -0,0 +1,20 @@
+public class T1 {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 1;
+        if(a % 2 == 0){
+            b = 3;
+        }
+        else{
+            b = 5;
+        }
+        if(b==1){
+            a = 2;
+        }
+        else{
+            a = 3;
+        }
+        c = 1;
+        System.out.println(c);
+    }
+}
