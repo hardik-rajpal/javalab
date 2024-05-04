59a37b4fa75e5ea7402730334a417f8e07cdd5b7
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-05-04 20:14:44 +0530

    sootup skeleton

--- src/main/java/com/rajpal/app/App.java
+++ src/main/java/com/rajpal/app/App.java
@@ -1,12 +1,12 @@
 package com.rajpal.app;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 import sootup.core.inputlocation.AnalysisInputLocation;
-import sootup.core.transform.BodyInterceptor;
 import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
 import sootup.java.core.JavaSootClass;
+import sootup.java.core.JavaSootMethod;
 import sootup.java.core.views.JavaView;
 
 /**
@@ -15,20 +15,26 @@ import sootup.java.core.views.JavaView;
  */
 public class App 
 {
+    public static void processClazz(JavaSootClass clazz){
+        System.out.println("Processing "+clazz.getName());
+        Set<JavaSootMethod> methods = clazz.getMethods();
+        
+        for(JavaSootMethod method:methods){
+            processMethod(method);
+        }
+    }
+    private static void processMethod(JavaSootMethod method) {
+        System.out.println(method.getDeclaringClassType().getClassName()+":"+method.getName());
+    }
     public static void main( String[] args )
     {
         String tcdir = "src/testcases";
         // AnalysisInputLocation inputLocation = new JavaSourcePathAnalysisInputLocation(tcdir);
         AnalysisInputLocation inputLocation = new JavaClassPathAnalysisInputLocation(tcdir);
         JavaView view = new JavaView(inputLocation);
-        List<BodyInterceptor> bodyInterceptors = inputLocation.getBodyInterceptors();
-        for(BodyInterceptor bodyInterceptor:bodyInterceptors){
-            System.out.println(bodyInterceptor.toString());
-        }
-        System.out.println("size of bil: "+bodyInterceptors.size());
          Collection<JavaSootClass> classes = view.getClasses();
          for(JavaSootClass clazz:classes){
-            System.out.println(clazz.getName().toString());
+            processClazz(clazz);
          }
     }
 }
